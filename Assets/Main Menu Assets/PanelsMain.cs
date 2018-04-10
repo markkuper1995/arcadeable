c4a009fac8a436cc445b450f2413d0ba2b192359
martinbdv <martinbdv@gmail.com>
2018-04-10 13:06:26 +0200

    Menu-Play start nu game
    Menu gaat nu naar Infinite runner

---
+++ Assets/Main Menu Assets/PanelsMain.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelsMain : MonoBehaviour {
+	public GameObject panel;
+
+	void Start() {
+		panel.SetActive(false);
+		panel.layer = 30;
+	}
+
+	public void ShowPanel() {
+		panel.SetActive (true);
+	}
+
+	public void HidePanel() {
+		panel.SetActive (false);
+	}
+}
---
+++ Assets/Main Menu Assets/PauseMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour {
+
+	public GameObject pauseMenuPanel;
+	public Text highScore;
+	public Text currentScore;
+	public int score = 0;
+
+	// Use this for initialization
+	void Start () {
+		pauseMenuPanel.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currentScore.text = score.ToString();
+	}
+
+	public void HidePauseMenu() {
+		pauseMenuPanel.SetActive (false);
+	}
+
+	public void ShowPauseMenu() {
+		pauseMenuPanel.SetActive (true);
+	}
+}
