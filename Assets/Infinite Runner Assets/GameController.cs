a13c8c63f8ad01cecc44f5af5a289cc1198a60b4
martinbdv <martinbdv@gmail.com>
2018-04-17 13:13:51 +0200

    Koppeling menu game

---
+++ Assets/Infinite Runner Assets/GameController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+	
+	public void StartInfiniteRunner() {
+		SceneManager.LoadScene("Infinite Runner", LoadSceneMode.Single);
+	}
+
+}
