7d6fad59a34aa602655684598af3d6684a7a0446
martinbdv <martinbdv@gmail.com>
2018-04-18 10:59:17 +0200

    DeathMenu gemaakt

--- Assets/Infinite Runner Assets/GameController.cs
+++ Assets/Infinite Runner Assets/GameController.cs
@@ -4,9 +4,34 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour {
-	
+
+	public GameObject panel;
+
 	public void StartInfiniteRunner() {
+		Time.timeScale = 1;
 		SceneManager.LoadScene("Infinite Runner", LoadSceneMode.Single);
 	}
 
+	public void ReturnToMainMenu() {
+		SceneManager.LoadScene ("MainMenu", LoadSceneMode.Single);
+	}
+
+	public void RestartGame() {
+		StartInfiniteRunner();
+	}
+
+	public void ResumeGame() {
+		panel.SetActive (false);
+		Time.timeScale = 1;
+	}
+
+	public void PauseGame() {
+		panel.SetActive (true);
+		Time.timeScale = 0;
+	}
+
+	public void Death(){
+		Time.timeScale = 0;
+		panel.SetActive (true);
+	}
 }
