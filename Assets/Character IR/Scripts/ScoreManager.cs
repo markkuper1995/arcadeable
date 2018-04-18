cb4422a86e9aec399bb793a92b45536f69e39b15
Mark <markkuper1995@gmail.com>
2018-04-18 12:09:39 +0200

    Revert "Revert "Added Death/Coins/Score""
    This reverts commit c433ae240655de55c63a9e2f0fcabb93b2cfe477.

---
+++ Assets/Character IR/Scripts/ScoreManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+    
+    public Text scoreText;
+    public Text highscoreText;
+    public Text coinText;
+
+    public float scoreCount;
+    public float highscoreCount;
+    public int coinCount;
+
+    public float pointsPerSecond;
+
+    public bool scoreIncreasing;
+    
+	// Use this for initialization
+	void Start () {
+		if(PlayerPrefs.HasKey("Highscore"))
+        {
+            highscoreCount = PlayerPrefs.GetFloat("Highscore");
+        }
+
+        coinCount = PlayerPrefs.GetInt("Coins");
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+        if (scoreIncreasing)
+        {
+            scoreCount += pointsPerSecond * Time.deltaTime;
+        }
+
+        if (scoreCount > highscoreCount)
+        {
+            highscoreCount = scoreCount;
+            PlayerPrefs.SetFloat("Highscore", highscoreCount);
+        }
+
+		scoreText.text = "Score: " + Mathf.Round(scoreCount);
+        highscoreText.text = "Highscore: " + Mathf.Round(highscoreCount);
+        coinText.text = "Coins: " + coinCount;
+        PlayerPrefs.SetInt("Coins", coinCount);
+	}
+
+    public void AddCoins(int coinsToAdd)
+    {
+        coinCount += coinsToAdd;
+    }
+}
