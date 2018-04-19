64ad6510d91c4f766139b5a2351d0a20eccdc495
martinbdv <martinbdv@gmail.com>
2018-04-19 11:01:14 +0200

    Pause en Death menu weer toegevoegd

---
+++ Assets/Infinite Runner Assets/MenuController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+
+	public GameObject panel;
+
+	public void BackToMainMenu() {
+		SceneManager.LoadScene ("MainMenu");
+	}
+
+	public void RestartGame(){
+		SceneManager.LoadScene( SceneManager.GetActiveScene().name );
+		Time.timeScale = 1;
+	}
+
+	public void ResumeGame() {
+		Time.timeScale = 1;
+		panel.SetActive (false);
+	}
+
+	public void PauseGame() {
+		Time.timeScale = 0;
+		panel.SetActive (true);
+	}
+}
---
+++ Assets/MainAssets/PanelsMain.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelsMain : MonoBehaviour {
+
+	public GameObject panel;
+
+	// Use this for initialization
+	void Start () {
+		panel.SetActive (false);
+	}
+	
+	public void ShowPanel() {
+		panel.SetActive (true);
+	}
+
+	public void HidePanel(){
+		panel.SetActive (false);
+	}
+}
