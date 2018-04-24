8a3936211a760cf7799e95663a9fe6a7b897b183
martinbdv <martinbdv@gmail.com>
2018-04-24 15:26:52 +0200

    Shop + LogoGame

---
+++ Assets/Character IR/Scripts/ShopManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopManager : MonoBehaviour {
+
+	public Button[] buttons;
+	public Text AmountCoins;
+
+	// Use this for initialization
+	void Start () {
+		this.SetCoins();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void SetCoins() {
+		AmountCoins.text = PlayerPrefs.GetInt( "Coins", 0 ).ToString();
+	}
+
+	public void ToggleButton(Button button) {
+		int n;
+		if (int.TryParse (button.GetComponentInChildren<Text> ().text, out n)) {
+			int amount = PlayerPrefs.GetInt( "Coins", 0 );
+			if (amount > n) {
+				foreach (Transform child in button.transform) {
+					if (child.name == "Text") {
+						child.GetComponent<Text> ().text = "Use";
+					} else {
+						Destroy (child.gameObject);
+					}
+				}
+				//button.GetComponentInChildren<Text> ().text = "Use";
+				PlayerPrefs.SetInt ( "Coins", amount-n );
+				this.SetCoins();
+			}
+		}
+		foreach (Button btn in buttons) {
+			if (button.GetComponentInChildren<Text> ().text == "Use" && btn.GetComponentInChildren<Text> ().text == "In use") {
+				btn.GetComponentInChildren<Text> ().text = "Use";
+				button.GetComponentInChildren<Text> ().text = "In use";
+			}
+		}
+	}
+}
