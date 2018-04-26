2c90c878bb7e363ef2932ba47e45d62d6a558f23
martinbdv <martinbdv@gmail.com>
2018-04-26 10:57:55 +0200

    Wat aangepast

---
+++ Assets/MainAssets/HighscoreManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine.UI;
+using UnityEngine;
+
+namespace AssemblyCSharp
+{
+	public class HighscoreManager : MonoBehaviour
+	{
+		public Text highscoreText;
+
+		void Start() {
+			if(PlayerPrefs.HasKey("Highscore"))
+			{
+				highscoreText.text = Mathf.Round(PlayerPrefs.GetFloat("Highscore")).ToString();
+			}
+		}
+	}
+}
+
