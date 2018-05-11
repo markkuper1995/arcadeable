f6aaf02d84aaad747c48a7a522f981e392d9e428
martinbdv <martinbdv@gmail.com>
2018-05-11 16:18:42 +0200

    Er worden nu random objecten toegevoegd in de achtergrond

---
+++ Assets/Character IR/Scripts/BgObjectGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BgObjectGenerator : MonoBehaviour {
+
+	private Transform generationPoint = null;
+	public GameObject[] backgrounds;
+	private int lastIndex = 0;
+
+	// Use this for initialization
+	void Start () {
+		generationPoint = GameObject.Find("BackgroundGenerationPoint").transform;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (generationPoint != null && transform.position.x < generationPoint.position.x) {
+			int index = Random.Range (0, 5);
+			if (index == lastIndex)
+				index++;
+			GameObject newBackground = Instantiate(backgrounds[index]);
+
+			transform.position = new Vector3(transform.position.x + Random.Range(10,25), backgrounds[index].transform.position.y, 1);
+			newBackground.transform.position = transform.position;
+			lastIndex = index;
+		}
+	}
+}
